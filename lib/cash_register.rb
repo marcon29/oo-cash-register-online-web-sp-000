675d620cf7c44aadc298116c00047861b9233e0a
Mark Stabler <mark@markstabler.com>
2020-03-17 15:23:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 class CashRegister
   attr_accessor :item, :discount
 
-  def initialize(discount=0)
-    @total = 0*(discount/100.0)
+  def initialize(employee_discount=0)
+    @total = 0
+    discount = (employee_discount/100.0)
   end
 end
