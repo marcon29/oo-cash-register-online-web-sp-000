d6f3ceae840236936a78eab6e94ef1e13e925e97
Mark Stabler <mark@markstabler.com>
2020-03-17 15:58:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount
-    total_dis = @total*(@discount/100.00)
+    total_dis = @total*(@discount/100)
     @total -= total_dis
     "After the discount, the total comes to $#{@total}."
   end
