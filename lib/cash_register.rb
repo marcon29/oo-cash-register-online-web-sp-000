30517d0a42afdbaaec08c51e94adde02eefd9a4a
Mark Stabler <mark@markstabler.com>
2020-03-17 15:27:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
 
   def initialize(employee_discount=0)
     @total = 100
-    discount = (employee_discount/100.0)
+    @discount = employee_discount
     puts "Total w/ discount is #{@total*discount}."
   end
 end
