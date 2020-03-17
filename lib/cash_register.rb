c1d40429f2c2b78d5d2eca865ced2240e637b0d8
Mark Stabler <mark@markstabler.com>
2020-03-17 15:28:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,8 +2,8 @@ class CashRegister
   attr_accessor :item, :discount
 
   def initialize(employee_discount=0)
-    @total = 100
+    @total = 0
     @discount = employee_discount
-    puts "Total w/ discount is #{@total*discount}."
+    #puts "Total w/ discount is #{@total*discount}."
   end
 end
